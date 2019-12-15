a7e2b1ef4f4bfaac28fca52762846b2b3922a7e1
Usman Akram <usman.akram@gmail.com>
2019-12-15 11:55:47 +0500

    lecture 5

---
+++ Lecture 5 15-12-2019/Very Basic JS/myFirstJS.js
@@ -0,0 +1,22 @@
+// alert("Hello Class");
+// alert("Hello Class1");
+
+// console.log("Hello from sonsole.log");
+// console.log("Hello from sonsole.log 1");
+// console.log("Hello from sonsole.log 2");
+
+var x, y, z;
+x = 5;
+y = 9;
+z = x + y;
+var first = "Usman";
+var second = "Akram";
+full = first + " :__ __    " + second;
+console.log(full);
+
+console.log(5 + "Hello");
+console.log(5 + 5 + "5");
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(true && false);
+console.log(true || false);
